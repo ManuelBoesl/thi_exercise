efdb86377d079db9efdc1c785a14afa8bf9341af
ManuelBoesl <manuel.boesl@gmail.com>
2022-10-20 22:43:18 +0200

    added slice version

--- exercise_4d.py
+++ exercise_4d.py
@@ -11,21 +11,39 @@ def version_regex(original_string, search_string):
 
     length = 0
 
-    for matchNum, match in enumerate(matches, start=1):
-        
-        print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-        length = matchNum
+    for match_num, match in enumerate(matches, start=1):
+        print_match(match_num, match.start(), match.end())
+        length = match_num
 
-    print(f"Found {length} matches")
+    print(f"{search_string} kommt in {original_string} {length} Mal vor\n")
 
 
 def version_slice(original_string, search_string):
-    pass
+    changed_string = original_string
+   
+    counter = 0
+    number_sliced = 0
+
+    while (start_index := changed_string.find(search_string)) != -1:
+        counter += 1
+        end_index = start_index + len(search_string)
+        changed_string = changed_string[end_index:len(changed_string)]
+        print_match(counter, start_index + number_sliced, end_index + number_sliced)
+        number_sliced += len(search_string)
+
+    print(f"{search_string} kommt in {original_string} {counter} Mal vor\n")    
+
+
+def print_match(match_num, start, end):
+    print (f"Match {match_num} was found at {start}-{end}.")
+
+
     
 
 if __name__ == '__main__':
-    original_string = input("Original: ")
-    search_string = input("Search: ")
-
+    original_string = input("String: ")
+    search_string = input("Teilstring: ")
+    print("Running version regex")
     version_regex(original_string, search_string)
+    print("running version slice")
     version_slice(original_string, search_string)
